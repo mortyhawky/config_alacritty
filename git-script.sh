48fe605a8e54d9249254f83d8b7071afa338d24a
mortyhawky <morty.hawky@outlook.com>
2026-04-24 08:24:38 +0200

    2026-04-24T08:24:38+02:00

--- git-script.sh
+++ git-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
+#Updated ArchReal 2026-04-24 08:23 Fri
 
-gh auth status
 git status
-git add .
+git add -Av
 
-git commit -m "$(date -Iseconds)"
-git push
+git commit -m "$(date -Iseconds)" -v
+git push -v
